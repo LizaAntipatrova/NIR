73a7936edfbc24c987a2973797444f164553d774
Liza Antipatrova <Liza.Antipa@gmail.com>
2025-05-17 13:36:03 +0300

    initial

---
+++ src/main/java/com/strongkittens/nirstorage/NirStorageApplication.java
@@ -0,0 +1,13 @@
+package com.strongkittens.nirstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NirStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NirStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strongkittens/nirstorage/NirStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strongkittens.nirstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NirStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
