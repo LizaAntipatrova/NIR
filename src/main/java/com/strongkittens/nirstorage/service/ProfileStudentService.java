12f83ec1fae414884eb8e35c0443fa1592f5b7b8
Liza Antipatrova <Liza.Antipa@gmail.com>
2025-05-18 14:48:11 +0300

    ProfileStudentService add getStudentsProjects

--- src/main/java/com/strongkittens/nirstorage/service/ProfileStudentService.java
+++ src/main/java/com/strongkittens/nirstorage/service/ProfileStudentService.java
@@ -1,16 +1,22 @@
 package com.strongkittens.nirstorage.service;
 
 import com.strongkittens.nirstorage.data.entity.Student;
+import com.strongkittens.nirstorage.dto.ProjectDTO;
 import com.strongkittens.nirstorage.dto.StudentDTO;
+import com.strongkittens.nirstorage.service.converter.ProjectToProjectDTOConverter;
 import com.strongkittens.nirstorage.service.converter.StudentToStudentDTOConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProfileStudentService {
 
     private final StudentService studentService;
+    private final ProjectService projectService;
 
     public StudentDTO getStudentDTO(Long id){
         Student foundStudent = studentService.findStudentById(id);
@@ -38,4 +44,11 @@ public class ProfileStudentService {
 
         studentService.save(student);
     }
+    public List<ProjectDTO> getStudentsProjects(Long id){
+        Student foundStudent = studentService.findStudentById(id);
+        return foundStudent.getProjects().stream()
+                .map(ProjectToProjectDTOConverter::convertProjectToProjectDTO)
+                .toList();
+    }
+
 }
