b006dadec76aa26131d0e5088ba488584bec1fe0
Liza Antipatrova <Liza.Antipa@gmail.com>
2025-05-18 14:07:30 +0300

    + Converter

---
+++ src/main/java/com/strongkittens/nirstorage/service/converter/ProjectToProjectDTOConverter.java
@@ -0,0 +1,19 @@
+package com.strongkittens.nirstorage.service.converter;
+
+import com.strongkittens.nirstorage.data.entity.Project;
+import com.strongkittens.nirstorage.dto.ProjectDTO;
+
+public class ProjectToProjectDTOConverter {
+    public static ProjectDTO convertProjectToProjectDTO(Project project){
+        ProjectDTO projectDTO = new ProjectDTO();
+        projectDTO.setId(project.getId());
+        projectDTO.setName(project.getName());
+        projectDTO.setDescription(project.getDescription());
+        projectDTO.setFile(project.getFile());
+        projectDTO.setGrade(project.getGrade());
+        projectDTO.setTeacherName(project.getTeacher().getFirstName());
+        projectDTO.setKeyWords(project.getKeyWords());
+        projectDTO.setPublicationDate(project.getPublicationDate());
+        return  projectDTO;
+    }
+}
