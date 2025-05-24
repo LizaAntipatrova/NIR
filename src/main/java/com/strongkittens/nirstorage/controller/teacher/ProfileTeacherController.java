76785490d3f57e0c17506533411e98007a07b13d
rubberPlant256 <ihrustal@yandex.ru>
2025-05-24 19:03:26 +0300

    выпадающий список для должностей преподавателей, сделано по красоте

--- src/main/java/com/strongkittens/nirstorage/controller/teacher/ProfileTeacherController.java
+++ src/main/java/com/strongkittens/nirstorage/controller/teacher/ProfileTeacherController.java
@@ -1,5 +1,6 @@
 package com.strongkittens.nirstorage.controller.teacher;
 
+import com.strongkittens.nirstorage.data.entity.Job;
 import com.strongkittens.nirstorage.dto.TeacherDTO;
 import com.strongkittens.nirstorage.uiservice.teacher.ProfileUITeacherService;
 import lombok.Getter;
@@ -8,11 +9,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/teacher/profile")
 public class ProfileTeacherController {
     private final ProfileUITeacherService profileUITeacherService;
+
+    private static final Map<Job, String> JOB_TITLES = new LinkedHashMap<>();
+
+    static {
+        JOB_TITLES.put(Job.ASSISTANT, "Ассистент");
+        JOB_TITLES.put(Job.ASSOCIATE_PROFESSOR, "Доцент");
+        JOB_TITLES.put(Job.SENIOR_LECTURER, "Старший преподаватель");
+        JOB_TITLES.put(Job.PROFESSOR, "Профессор");
+    }
 
     @GetMapping("")
     public String showProfileForm(Model model, @RequestHeader("Cookie") String cookieHeader){
@@ -21,6 +34,7 @@ public class ProfileTeacherController {
 
     @GetMapping("/edit")
     public String showEditForm(Model model, @RequestHeader("Cookie") String cookieHeader){
+        model.addAttribute("jobTitles", JOB_TITLES);
         return profileUITeacherService.getProfileEditPage(model, cookieHeader);
     }
 //TODO:сделай выпадающий список для профессий!!!!!
